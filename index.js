14a6609cf95b5ff0430d5531cd1abaf045c6647f
Charlie Gray <charliechasegray@gmail.com>
2022-12-25 17:28:59 -0700

    commit tree 997ae434f260b206c9d6b6f08c500322dde5b3cb

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+}
+function appendCat(name) {
+    const moreCats = [...cats, "Broom"];
+    return moreCats;
+}
+function prependCat(name) {
+    const evenMoreCats = ["Arnold", ...cats];
+    return evenMoreCats;
+}
+function removeLastCat(name) {
+    const lessCats = cats.slice(0, -1);
+    return lessCats;
+}
+function removeFirstCat(name) {
+    const evenLessCats = cats.slice(1);
+    return evenLessCats;
+}
